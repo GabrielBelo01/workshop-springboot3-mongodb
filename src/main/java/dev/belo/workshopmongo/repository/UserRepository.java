9d8f044cd743448be9aa21e30f21a231f7fb2aaf
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2025-11-27 17:14:41 -0300

    Conectando o MOngoDB com o repostiory e service

---
+++ src/main/java/dev/belo/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package dev.belo.workshopmongo.repository;
+
+import dev.belo.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/dev/belo/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package dev.belo.workshopmongo.services;
+
+import dev.belo.workshopmongo.domain.User;
+import dev.belo.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
