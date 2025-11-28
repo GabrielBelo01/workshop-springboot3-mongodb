8945184bfc494bb8f30367d621ea0cb3759cc975
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2025-11-28 17:09:39 -0300

    Referenciando os posts do usuario

--- src/main/java/dev/belo/workshopmongo/domain/User.java
+++ src/main/java/dev/belo/workshopmongo/domain/User.java
@@ -1,10 +1,13 @@
 package dev.belo.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -16,6 +19,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User(){}
 
@@ -49,6 +55,10 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    public List<Post> getPosts() {
+        return posts;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -60,4 +70,6 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+
 }
