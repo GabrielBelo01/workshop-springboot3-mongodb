99518afe32837776f552f82c101349c7f52673a5
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2025-11-28 14:35:42 -0300

    Atualizacao de usuario com PUT

--- src/main/java/dev/belo/workshopmongo/resources/UserResource.java
+++ src/main/java/dev/belo/workshopmongo/resources/UserResource.java
@@ -49,5 +49,13 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update (@RequestBody UserDTO objDto, @PathVariable String id){
+        User obj = service.FromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+
+        return ResponseEntity.noContent().build();
+    }
 
 }
