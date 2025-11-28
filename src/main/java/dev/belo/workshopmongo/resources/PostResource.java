db51017188069f24ed28080774828802e0eacc0b
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2025-11-28 17:45:54 -0300

    obtendo post por id

---
+++ src/main/java/dev/belo/workshopmongo/resources/PostResource.java
@@ -0,0 +1,33 @@
+package dev.belo.workshopmongo.resources;
+
+import dev.belo.workshopmongo.domain.Post;
+import dev.belo.workshopmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping
+    public ResponseEntity<List<Post>> findAll(){
+        List<Post> list = service.findAll();
+        return  ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post post = service.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+
+}
---
+++ src/main/java/dev/belo/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package dev.belo.workshopmongo.services;
+
+import dev.belo.workshopmongo.domain.Post;
+import dev.belo.workshopmongo.repository.PostRepository;
+import dev.belo.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public List<Post> findAll(){
+        return repo.findAll();
+    }
+
+    public Post findById(String id){
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
