b16ca396ad20b211e9cda34c215a378815c45c65
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2025-11-28 11:05:45 -0300

    Obtendo Usuario por Id

---
+++ src/main/java/dev/belo/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package dev.belo.workshopmongo.resources.exception;
+
+import dev.belo.workshopmongo.services.exception.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(System.currentTimeMillis(),status.value(),"Nao Encontrado", e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/dev/belo/workshopmongo/services/UserService.java
+++ src/main/java/dev/belo/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package dev.belo.workshopmongo.services;
 
 import dev.belo.workshopmongo.domain.User;
 import dev.belo.workshopmongo.repository.UserRepository;
+import dev.belo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
