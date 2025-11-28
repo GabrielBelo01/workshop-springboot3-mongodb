029fb6b2f19c9f9e8c3140f5c2880d77b7b91c74
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2025-11-28 11:55:00 -0300

    Insercao de usuario com POST

--- src/main/java/dev/belo/workshopmongo/services/UserService.java
+++ src/main/java/dev/belo/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package dev.belo.workshopmongo.services;
 
 import dev.belo.workshopmongo.domain.User;
+import dev.belo.workshopmongo.dto.UserDTO;
 import dev.belo.workshopmongo.repository.UserRepository;
 import dev.belo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert (User obj){
+        return repo.insert(obj);
+    }
+
+    public User FromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
